b8314dcf4b949f956531c6bdcaf21f866d8d786d
David Crotty <david.crotty.work@gmail.com>
2018-12-02 16:41:31 +0000

    Realm dependencies

---
+++ app/src/main/java/net/davidcrotty/realmpsntest/App.kt
@@ -0,0 +1,11 @@
+package net.davidcrotty.realmpsntest
+
+import android.app.Application
+import io.realm.Realm
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
